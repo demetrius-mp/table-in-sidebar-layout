ea66ec7096d93355c3edee065a4920c729301a7d
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-11-03 12:09:40 -0400

    repro

---
+++ src/routes/without-sidebar/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import AppTable from '$lib/components/app-table.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+</script>
+
+<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+	<Breadcrumb.Root>
+		<Breadcrumb.List>
+			<Breadcrumb.Item class="hidden md:block">
+				<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator class="hidden md:block" />
+			<Breadcrumb.Item>
+				<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+			</Breadcrumb.Item>
+		</Breadcrumb.List>
+	</Breadcrumb.Root>
+</header>
+<div class="flex flex-1 flex-col gap-4 p-4">
+	<AppTable />
+</div>
